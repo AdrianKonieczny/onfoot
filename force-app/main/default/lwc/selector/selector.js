6b19e00922008494ab3a438710c8c1cf333bf148
Adrian Konieczny <adriankon@outlook.com>
2023-02-02 18:37:13 +0100

    Custom labels added to lwc

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,7 +1,12 @@
 import { LightningElement } from 'lwc';
+import Available_Products from '@salesforce/label/c.Available_Products';
+import Select_a_shoe from '@salesforce/label/c.Select_a_shoe';
 
 export default class Selector extends LightningElement {
     selectedProduct;
+    availableProducts = Available_Products;
+    selectShoe = Select_a_shoe;
+    
 
     handleProductSelected(evt) {
         this.selectedProduct = evt.detail.product;
